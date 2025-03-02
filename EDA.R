c278068b93bc1c9b7f74b7da5e5218d6afab652d
Alexander Salm <asalm@ualberta.ca>
2025-03-02 03:39:41 -0700

    added some plots for yearly income and age

--- EDA.R
+++ EDA.R
@@ -26,6 +26,7 @@ process_yearly_income <- function (arr){
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(lubridate)
 
 customers      <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Customers.csv")
 products       <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Products.csv")
@@ -36,14 +37,43 @@ transactions97 <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Transact
 transactions98 <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Transactions_1998.csv")
 
 # Clean up customer yearly income, split into seperate columns
-customers_yrincome <- separate(customers, col = yearly_income, into = c("yearly_income_min", "yearly_income_max"), sep = " - ")
+customers_clean <- separate(customers, col = yearly_income, into = c("yearly_income_min", "yearly_income_max"), sep = " - ")
 
 # Remove entries with NA in yearly income (about 200 entries out of 10000)
-customers_yrincome <- customers_yrincome[!is.na(customers_yrincome$yearly_income_max),]
-customers_yrincome <- customers_yrincome[!is.na(customers_yrincome$yearly_income_min),]
+customers_clean <- customers_clean[!is.na(customers_clean$yearly_income_max),]
+customers_clean <- customers_clean[!is.na(customers_clean$yearly_income_min),]
 
 # Remove $, K (if exists, * by 1000) and convert to numeric
-customers_yrincome$yearly_income_max <- process_yearly_income(customers_yrincome$yearly_income_max)
-customers_yrincome$yearly_income_min <- process_yearly_income(customers_yrincome$yearly_income_min)
+customers_clean$yearly_income_max <- process_yearly_income(customers_clean$yearly_income_max)
+customers_clean$yearly_income_min <- process_yearly_income(customers_clean$yearly_income_min)
 
-#ggplot(customers) + geom_smooth(mapping=aes(x=))
+# Convert columns to correct datatypes
+customers_clean$member_card <- factor(customers_clean$member_card)
+customers_clean$education <- factor(customers_clean$education)
+customers_clean$customer_country <- factor(customers_clean$customer_country)
+customers_clean$occupation <- factor(customers_clean$occupation)
+
+# Add an age column (as of Jan 01, 2025)
+customers_clean$birthdate <- parse_date_time(customers_clean$birthdate, "m/d/y", tz="") #dmy - day, month, year HM - hour, minute
+customers_clean$age <- as.double(difftime(parse_date_time("01/01/2025", "m/d/y", tz=""), customers_clean$birthdate, unit=c("days")))
+
+# Some yearly income plots
+#ggplot(customers_clean) + geom_histogram(mapping=aes(x=yearly_income_max))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=member_card))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=education))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=customer_country))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=occupation))
+
+# Some age plots
+#ggplot(customers_clean) + geom_histogram(mapping=aes(x=age))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=member_card))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=education))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=customer_country))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=occupation))
+
+
+
+
+
+
+
