17bf44dd23b816e838944ecff1ce82f17a24f404
Alexander Salm <asalm@ualberta.ca>
2025-03-03 00:48:01 -0700

    added plots for seasonal transactions and returns

--- EDA.R
+++ EDA.R
@@ -22,12 +22,25 @@ process_yearly_income <- function (arr){
   return(ret)
 }
 
+add_purchases_columns <- function(customers, purchases){
+  purchase_count <- c()
+  for(i in 1:length(customers$customer_id)){
+    id <- customers$customer_id[i]
+    
+    purchase_count <- append(purchase_count, sum(filter(purchases, customer_id==id)$quantity))
+  }
+  
+  customers$purchase_count <- purchase_count
+  return(customers)
+}
+
 # Import libraries
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(lubridate)
 
+# Load data
 customers      <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Customers.csv")
 products       <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Products.csv")
 regions        <- read.csv("MavenMarket CSV Files/CSV Files/MavenMarket_Regions.csv")
@@ -57,12 +70,47 @@ customers_clean$occupation <- factor(customers_clean$occupation)
 customers_clean$birthdate <- parse_date_time(customers_clean$birthdate, "m/d/y", tz="") #dmy - day, month, year HM - hour, minute
 customers_clean$age <- as.double(difftime(parse_date_time("01/01/2025", "m/d/y", tz=""), customers_clean$birthdate, unit=c("days")))
 
+# Create unified transactions
+transactions <- rbind(transactions97, transactions98)
+
+# Add month column to transactions
+transactions$transaction_date <- parse_date_time(transactions$transaction_date, "m/d/y", tz="")
+transactions$month <- factor(month(transactions$transaction_date))
+
+# Add month column to returns
+returns$return_date <- parse_date_time(returns$return_date, "m/d/y", tz="")
+returns$month <- factor(month(returns$return_date))
+
+# Plots for transactions by month
+ggplot(transactions) + geom_bar(mapping=aes(x=month)) + labs(x="Month", y="Transaction Count") + ggtitle("Individual transactions (not accounting for quantity)")
+
+# Plots for transaction quantities by month
+transaction_quantities <- aggregate(transactions$quantity, by=list(Category=transactions$month), FUN=sum)
+ggplot(transaction_quantities) + geom_bar(mapping=aes(x=Category, y=x), stat="identity") + labs(x="Month", y="Transaction Quantity") + ggtitle("Total Items Purchased (accounting for quantity)")
+
+# Plots for transactions by month
+ggplot(returns) + geom_bar(mapping=aes(x=month)) + labs(x="Month", y="Return Count") + ggtitle("Individual return instances (not accounting for quantity)")
+
+# Plots for transaction quantities by month
+return_quantities <- aggregate(returns$quantity, by=list(Category=returns$month), FUN=sum)
+ggplot(return_quantities) + geom_bar(mapping=aes(x=Category, y=x), stat="identity") + labs(x="Month", y="Return Quantity") + ggtitle("Total Items Returned (accounting for quantity)")
+
+
+# Add purchase columns (takes a long time to run!)
+#customers_clean <- add_purchases_columns(customers_clean, transactions)
+
 # Some yearly income plots
 #ggplot(customers_clean) + geom_histogram(mapping=aes(x=yearly_income_max))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=member_card))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=education))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=customer_country))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=occupation))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=gender))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=total_children, group=total_children))
+#ggplot(customers_clean) + geom_boxplot(mapping=aes(x=yearly_income_max, y=num_children_at_home, group=num_children_at_home))
+#ggplot(customers_clean) + geom_count(mapping=aes(x=yearly_income_max, y=purchase_count))
+
+#ggplot(customers_clean) + geom_point(mapping=aes(x=yearly_income_min, y=yearly_income_max))
 
 # Some age plots
 #ggplot(customers_clean) + geom_histogram(mapping=aes(x=age))
@@ -70,8 +118,8 @@ customers_clean$age <- as.double(difftime(parse_date_time("01/01/2025", "m/d/y",
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=education))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=customer_country))
 #ggplot(customers_clean) + geom_boxplot(mapping=aes(x=age, y=occupation))
-
-
+#ggplot(customers_clean) + geom_smooth(mapping=aes(x=age, y=yearly_income_max))
+#ggplot(customers_clean) + geom_smooth(mapping=aes(x=age, y=yearly_income_min))
 
 
 
